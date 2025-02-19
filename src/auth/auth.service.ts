f47ae6a5bb4628980c33b4924fbd1b1a661f249b
Carlos Jesús Sipan Lozano <carlossipanlozano@gmail.com>
2025-02-19 11:14:18 -0500

    add JWT authentication support with configuration and login token generation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,5 @@
 import {
+  HttpStatus,
   Injectable,
   OnModuleInit,
   UnauthorizedException,
@@ -7,6 +8,7 @@ import { LoginDto } from './dto/login.dto';
 import * as bcrypt from 'bcrypt';
 import { PrismaClient } from '@prisma/client';
 import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService extends PrismaClient implements OnModuleInit {
@@ -14,35 +16,42 @@ export class AuthService extends PrismaClient implements OnModuleInit {
     this.$connect();
   }
 
-  constructor(private readonly usersService: UsersService) {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {
     super();
   }
 
-  async login(LoginAuthDto: LoginDto) {
-    const { email, password } = LoginAuthDto;
+  async login(loginDto: LoginDto) {
+    const { email, password } = loginDto;
 
     try {
       const user = await this.usersService.findOneByEmail(email);
-
       if (!user) {
         throw new UnauthorizedException('Invalid email or password');
       }
 
       const isPasswordValid = await bcrypt.compare(password, user.password);
-
       if (!isPasswordValid) {
         throw new UnauthorizedException('Invalid email or password');
       }
 
+      const payload = { email: user.email };
+
+      const token = await this.jwtService.signAsync(payload);
+
       return {
-        email: user.email,
+        token: token,
         message: 'Login successful',
       };
     } catch (error) {
-      throw new UnauthorizedException('Invalid email or password');
+      throw new UnauthorizedException({
+        status: HttpStatus.BAD_REQUEST,
+        message: 'Error Interno del Servidor',
+      });
     }
   }
-  
 
   findAll() {
     return `This action returns all auth`;
